122c6e02a3e1dcd1853a1d43adcf6fa581045e08
Mohamed Saied <infint_hero@hotmail.com>
2024-12-29 20:22:10 +0200

    added some code

--- face_detction.cpp
+++ face_detction.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
+    // Create a window
+    cv::namedWindow("Example Window", cv::WINDOW_NORMAL);
+    cv::imshow("Example Window", cv::Mat::zeros(480, 640));
+    while(1)
+    {
+        
+    }
     return 0;
 }
